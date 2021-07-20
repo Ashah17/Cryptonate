d771ba9363173bc83b5fdcc3641a9687974042c0
Aryan Shah <aryanjshah@gmail.com>
2021-07-20 13:44:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import AccountInfo from './AccountInfo';
+import Contact from './Contact';
+import CreateDonation from './CreateDonation';
+import Home from './Home';
+import Navigation from './Navigation';
+import SpecificDonation from './SpecificDonation';
+import TeamPage from './TeamPage';
+import PortalPage from './PortalPage';
+import PreviousDonations from './PreviousDonations';
+import DonationStatistics from './DonationStatistics';
+import signin from './signin';
+import signup from './signup';
+import donationlisst from './donationlisst';
+import { Route, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className="App">
+      <Navigation />
+      <Route exact path="/" component = {Home} />
+      <Route exact path="/contact" component = {Contact} />
+      <Route exact path="/teampage" component = {TeamPage} />
+      <Route exact path="/accountinfo" component = {AccountInfo} />
+      <Route exact path="/specificdonation" component = {SpecificDonation} />
+      <Route exact path="/signin" component = {signin} />
+      <Route exact path="/signup" component = {signup} />
+      <Route exact path="/donationlisst" component = {donationlisst} />
+      
     </div>
-  );
-}
+};
 
 export default App;
---
+++ src/Navigation.js
@@ -0,0 +1,35 @@
+
+import NavCSS from './Navigation.module.css';
+import {Link} from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to ="/">Home</Link>
+        </li>
+        <li>
+        <Link to ="/teampage">Team</Link>
+        </li>
+        <li>
+        <Link to ="/specificdonation">Donations</Link>
+        </li>
+        <li>
+        <Link to ="/accountinfo">AccountInfo</Link>
+        </li>
+        <li>
+        <Link to ="/contact">Contact</Link>
+        </li>
+        <li style={{ float: 'right' }}>
+        <Link to ="/signin">Login</Link>
+        </li>
+        <li style={{ float: 'right' }}>
+        <Link to ="/signup">Sign up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
